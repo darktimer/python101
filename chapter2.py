bfc44f1401c7d9f233d15fdcb2b4690ef509e601
darktimer <darktimer.yz@outlook.com>
2020-10-30 21:17:55 +0800

    2020-10-30 21:17:51

---
+++ chapter2.py
@@ -0,0 +1,22 @@
+bicycles = ['trek','cannondale','redline','specialized']
+print(bicycles)
+print(bicycles[1])
+print(bicycles[1].title())
+print(bicycles[-1])
+
+message = "My first bicycle was a " + bicycles[0].title() + "."
+print(message)
+
+print("------------#3-1 to 3-3")
+names = ['harry','ron','hamanine']
+print(names[0].title())
+print(names[1].title())
+print(names[2].title())
+
+message = "Good morning！"
+print(message + names[0].title())
+print(message + names[1].title())
+print(message + names[2].title())
+
+wayToJob = ['Honda motorcycle','car','bicycle']
+print("I would like to own a " + wayToJob[0] + '.')
---
+++ helloworld.py
@@ -0,0 +1,67 @@
+print("helloworld")
+
+#2-1 2-2
+print("----------------\n#2-1 2-2")
+message="hello world"
+print(message)
+
+message="hello python world"
+print(message)
+
+#message="test"
+#print(mesage)
+print("----------------\n")
+
+name="hell python"
+print(name.title())
+print(name.upper())
+print(name.lower())
+
+first_name="hell"
+last_name="python"
+print(first_name+" "+last_name)
+
+name=first_name+" "+last_name
+new_message=message+" "+name.title()
+print(new_message+" aha! new message\n\n") 
+
+print("----------------\n#2-3 to 2-7")
+name="Harry Potter"
+print("Hi! "+name+", would you like to learn some Python today?")
+
+print(name.upper())
+print(name.lower())
+print(name.title())
+
+name="Albert Einstein"
+words="A person who never made a mistake never tried anything new."
+print( name + " once said, " + '"' + words + '"')
+
+name = "\t Albert Einstein \n"
+print(name)
+print(name.lstrip())
+print(name.rstrip())
+print(name.strip())
+print("----------------\n")
+
+print(3+2)
+print(3/2)
+print(0.2+0.1)
+print(0.1*2)
+print(0.1*0.1)
+print(0.1**0.1)
+print(2**3)
+
+age=23
+#message="Happy " + age + "rd Birthday"
+print(message)
+message="Happy " + str(age) + "rd Birthday"
+
+print("----------------\n#2-8 2-9")
+print(5 + 3)
+print(2 * 4)
+print(16 / 2)
+print(9 - 1)
+
+fav_num = 3
+print("my fav num is " + str(fav_num))
