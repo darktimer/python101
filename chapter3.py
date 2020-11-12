e7ce6561ab7e19fbf3b4dbb1575c360980e66a7e
darktimer <darktimer.yz@outlook.com>
2020-11-12 16:55:23 +0800

    。

---
+++ chapter3.py
@@ -0,0 +1,7 @@
+bicycle = ['trek','cannondale','redline','specialized']
+message = "My first bicycle was a " + bicycle[0].title()
+print(message)
+
+#3-1 to 3-3
+names = ['harry','ron','hermione']
+
